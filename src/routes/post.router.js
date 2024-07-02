60f99252b4e4b24cfba6eea46070456c283b6f6d
patrick espinoza <patrickespinoza38@gmail.com>
2024-07-01 20:03:26 -0600

    reintegrar el auth

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -6,7 +6,7 @@ const auth = require("../middlewares/auth.middlewares");
 
 const router = express.Router();
 
-router.post("/", async (request, response) => {
+router.post("/", auth, async (request, response) => {
   try {
     const postCreated = await postCase.createPost(request.body);
     response.json({
